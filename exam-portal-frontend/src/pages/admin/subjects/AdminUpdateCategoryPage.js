162d98e21deb030f8ab21e07ff78873bea8d78ed
shebibovic <sanida.hebibovic@edu.fit.ba>
2023-12-17 17:49:55 +0100

    fixed update subject

--- exam-portal-frontend/src/pages/admin/subjects/AdminUpdateCategoryPage.js
+++ exam-portal-frontend/src/pages/admin/subjects/AdminUpdateCategoryPage.js
@@ -14,49 +14,31 @@ const AdminUpdateCategoryPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const params = useParams();
-  const catId = params.catId;
+  const catId = params.subjectId;
 
   const oldCategory = useSelector((state) =>
-    state.categoriesReducer.categories.filter((cat) => cat.catId == catId)
+      state.categoriesReducer.categories.filter((cat) => cat.subjectId == catId)
   )[0];
   const [title, setTitle] = useState(oldCategory ? oldCategory.title : "");
   const [description, setDescription] = useState(
-    oldCategory ? oldCategory.description : ""
+      oldCategory ? oldCategory.description : ""
   );
   const token = localStorage.getItem("jwtToken");
-  const [selectedUser, setSelectedUser] = useState("");
-  const [users, setUsers] = useState([]);
-  const [selectedUsers, setSelectedUsers] = useState([]);
   const [professor, setProfessor] = useState([]);
-  const [selectedStudents, setSelectedStudents] = useState([]);
+  const [selectedUser, setSelectedUser] = useState("");
 
-  const submitHandler = async (e) => {
+  const submitHandler = (e) => {
     e.preventDefault();
-    const category = {
-      catId: catId,
-      title: title,
-      description: description,
-      userId: selectedUser,
-      students: selectedStudents,
-    };
-
-    try {
-      dispatch({ type: categoriesConstants.UPDATE_CATEGORY_REQUEST });
-      const updateResponse = await updateCategory(dispatch, category, token);
-      if (updateResponse.isUpdated) {
-        dispatch({
-          type: categoriesConstants.UPDATE_CATEGORY_SUCCESS,
-          payload: updateResponse.data,
-        });
-        swal("Subject Updated!", `${title} successfully updated`, "success");
+    const category = { catId: catId, title: title, description: description, userId: selectedUser };
+    updateCategory(dispatch, category, token).then((data) => {
+      if (data.type === categoriesConstants.UPDATE_CATEGORY_SUCCESS) {
+        swal("Category Updated!", `${title} succesfully updated`, "success");
+      } else {
+        swal("Category Not Updated!", `${title} not updated`, "error");
       }
-      navigate("/adminCategories");
-    } catch (error) {
-      console.error("Error updating category:", error);
-      // Handle error (e.g., show an error message)
-    }
+    });
+    navigate("/adminCategories");
   };
-
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -77,112 +59,71 @@ const AdminUpdateCategoryPage = () => {
         console.error("Error fetching users:", error);
       }
 
-      try {
-        const response = await fetch("/api/subject/students", {
-          headers: {
-            Authorization: `Bearer ${token}`, // Dodajte ovu liniju kako biste poslali token
-            "Content-Type": "application/json", // Ovisno o potrebi, možda trebate dodati i Content-Type
-          },
-        });
-        if (!response.ok) {
-          throw new Error("Failed to fetch students");
-        }
-        const usersData = await response.json();
-        console.log(usersData); // Log fetched user data
-        setUsers(usersData);
-      } catch (error) {
-        console.error("Error fetching students:", error);
-      }
     };
 
     fetchUsers();
   }, [dispatch, token]);
 
   return (
-    <div className="adminUpdateCategoryPage__container">
-      <div className="adminUpdateCategoryPage__sidebar">
-        <Sidebar />
+      <div className="adminUpdateCategoryPage__container">
+        <div className="adminUpdateCategoryPage__sidebar">
+          <Sidebar />
+        </div>
+        <div className="adminUpdateCategoryPage__content">
+          <FormContainer>
+            <h2>Update Category</h2>
+            <Form onSubmit={submitHandler}>
+              <Form.Group className="my-3" controlId="title">
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter Category Title"
+                    value={title}
+                    onChange={(e) => {
+                      setTitle(e.target.value);
+                    }}
+                ></Form.Control>
+              </Form.Group>
+
+              <Form.Group className="my-3" controlId="description">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                    style={{ textAlign: "top" }}
+                    as="textarea"
+                    rows="5"
+                    type="text"
+                    placeholder="Enter Category Description"
+                    value={description}
+                    onChange={(e) => {
+                      setDescription(e.target.value);
+                    }}
+                ></Form.Control>
+              </Form.Group>
+
+              <Form.Select
+                  aria-label="Choose Professor"
+                  onChange={(e) => setSelectedUser(e.target.value)}
+                  value={selectedUser}
+              >
+                <option value="">Choose Professor</option>
+                {professor.map((user) => (
+                    <option key={user.userId} value={user.userId}>
+                      {user.firstName} {user.lastName}
+                    </option>
+                ))}
+              </Form.Select>
+
+              <Button
+                  className="my-3 adminUpdateCategoryPage__content--button"
+                  type="submit"
+                  variant=""
+              >
+                Update
+              </Button>
+            </Form>
+          </FormContainer>
+        </div>
       </div>
-      <div className="adminUpdateCategoryPage__content">
-        <FormContainer>
-          <h2>Update Subject</h2>
-          <Form onSubmit={submitHandler}>
-            <Form.Group className="my-3" controlId="title">
-              <Form.Label>Title</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Subject Title"
-                value={title}
-                onChange={(e) => {
-                  setTitle(e.target.value);
-                }}
-              ></Form.Control>
-            </Form.Group>
-
-            <Form.Group className="my-3" controlId="description">
-              <Form.Label>Description</Form.Label>
-              <Form.Control
-                style={{ textAlign: "top" }}
-                as="textarea"
-                rows="5"
-                type="text"
-                placeholder="Enter Subject Description"
-                value={description}
-                onChange={(e) => {
-                  setDescription(e.target.value);
-                }}
-              ></Form.Control>
-            </Form.Group>
-            <Form.Select
-                aria-label="Choose Professor"
-                onChange={(e) => setSelectedUser(e.target.value)}
-                value={selectedUser}
-            >
-              <option value="">Choose Professor</option>
-              {professor.map((user) => (
-                  <option key={user.userId} value={user.userId}>
-                    {user.firstName} {user.lastName}
-                  </option>
-              ))}
-            </Form.Select>
-
-
-            <Form.Group controlId="students">
-              <Form.Label>Choose Students (Multiple)</Form.Label>
-              {users.map((users) => (
-                  <Form.Check
-                      key={users.userId}
-                      type="checkbox"
-                      id={`user-${users.userId}`}
-
-                      label={users.email}
-                      onChange={(e) => {
-                        const isChecked = e.target.checked;
-                        if (isChecked) {
-                          setSelectedStudents((prevSelected) => [...prevSelected, users.userId]);
-                        } else {
-                          setSelectedStudents((prevSelected) =>
-                              prevSelected.filter((selectedUserId) => selectedUserId !== users.userId)
-                          );
-                        }
-                      }}
-                      checked={selectedStudents.includes(users.userId)}
-                  />
-              ))}
-            </Form.Group>
-
-
-            <Button
-              className="my-3 adminUpdateCategoryPage__content--button"
-              type="submit"
-              variant=""
-            >
-              Update
-            </Button>
-          </Form>
-        </FormContainer>
-      </div>
-    </div>
   );
 };
 
