c359873b6a1b8e54ac3415e4f62f677e375c5d9d
Kerim Nurikic <knurikic1@etf.unsa.ba>
2023-12-24 14:48:49 +0100

    fix

--- exam-portal-backend/src/main/java/com/project/examportalbackend/ExamPortalBackendApplication.java
+++ exam-portal-backend/src/main/java/com/project/examportalbackend/ExamPortalBackendApplication.java
@@ -1,6 +1,6 @@
 package com.project.examportalbackend;
 
-import com.project.examportalbackend.services.AddStaticData;
+//import com.project.examportalbackend.services.AddStaticData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,16 +12,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class ExamPortalBackendApplication {
 
-	@Autowired
-	AddStaticData addStaticData;
+//	@Autowired
+//	AddStaticData addStaticData;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamPortalBackendApplication.class, args);
 	}
 
-	@Bean
-	public ApplicationRunner initializer() {
-		return args -> addStaticData.addStaticData();
-	}
+//	@Bean
+//	public ApplicationRunner initializer() {
+//		return args -> addStaticData.addStaticData();
+//	}
 
 }
